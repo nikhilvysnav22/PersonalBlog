709a9f1fadd17ed4a6f51db0c617dba4a136947d
Nikhil <nikhilrv46@gmail.com>
2021-06-26 15:49:44 -0500

    Db migration

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask , render_template , redirect, url_for, request
+from flask import Flask , render_template ,redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:No@localhost/PersonalBlog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Discover123!@localhost/blog'
 
 # initialize
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # To Db
 class PostTable(db.Model):
     sno = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-    date = db.Column(db.DATE, primary_key=False)
+    date = db.Column(db.DateTime, default= datetime.now())
     name = db.Column(db.String(120), unique=False, nullable=False)
     emailid = db.Column(db.String(50), unique=False, nullable=False)
     phone = db.Column(db.String(15), unique=False, nullable=False)
@@ -33,7 +35,7 @@ def about():
 def post():
     return render_template('post.html')
 
-@app.route("/contact" , methods = ['POST', 'GET'])
+@app.route("/contact" , methods=['POST', 'GET'])
 def contact():
     if request.method == 'POST' :
         # add entry to the database
@@ -46,9 +48,7 @@ def contact():
         entry_to_db = PostTable(name=name, emailid=emailid , phone=phone, message=message)
         db.session.add(entry_to_db)
         db.session.commit()
-
-        pass
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4013 , threaded =True)
+    app.run(debug=True, port=4015 , threaded=True)
